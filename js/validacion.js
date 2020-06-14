43c3d37e70826a4d1a1416fec890db80105e3e27
jorge volante facio <16065361@alumnos.icel.edu.mx>
2020-06-14 15:31:37 -0500

    funcion para longitud del nombre

---
+++ js/validacion.js
@@ -0,0 +1,12 @@
+//version 4 de jacacript
+
+function validar(formulario) {
+
+    if(formulario.nombre.value.length < 3) {
+        alert("Escriba por lo menos 3 caracteres en el campo de Nombre")
+        //focus() nos ayuda a focalizar donde esta el error del llenado 
+        //realiza un posicionamiento al elemento
+
+        formulario.nombre.focus();
+        return false;
+    }
