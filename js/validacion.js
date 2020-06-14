ab96d3dddee36df2bbd51781d6ddbb5081c84755
jorge volante facio <16065361@alumnos.icel.edu.mx>
2020-06-14 16:35:45 -0500

    validacion de solo digitos

--- js/validacion.js
+++ js/validacion.js
@@ -37,4 +37,37 @@ function validar(formulario) {
         return(false);
     }
 
+    if(formulario.edad.value.length < 2) {
+        alert("Escriba maximo 2 dijitos, o la maxima edad es de 99")
+        //focus() nos ayuda a focalizar donde esta el error del llenado 
+        //realiza un posicionamiento al elemento
+        formulario.edad.focus();
+        return false;
+    }
+    
+    var checkok = "1234567890";
+    var checkString = formulario.edad.value;
+
+    alert(checkString);
+
+    var aliValid = true;
+    for (var i =0; i < checkString.length; i++) {
+        //charAt nos ayuda a separar una cadena en caracteres
+        var ch = checkString.charAtg(i);
+        for (var j = 0; j <checkok.length; j++)
+        if ( ch == checkok.charAt(j))
+             break;
+        if( j == checkok.length ) {
+            aliValid = false;
+            break;
+        }
+
+            
+        }
+    //debo saber el estado del aliValid
+    if(!aliValid){
+        alert("Escriba solo digitos en el campo de la edad");
+        formulario.edad.focus();
+        return(false);
+    }
 }
